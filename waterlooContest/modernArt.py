95728397e31eb0b57e2e2a7c05a6484a537a5ba1
Jude Rozario <78745006+juderozario@users.noreply.github.com>
2022-10-22 06:12:48 -0400

    First Commit

--- waterlooContest/modernArt.py
+++ waterlooContest/modernArt.py
@@ -1,37 +1,24 @@
-rows = int(input())
-columns = int(input())
-num_of_changes = int(input())
-row_changes = []
-column_changes = []
-canvas = []
-for i in range(num_of_changes):
-    change = str(input())
-    if change[0] == "R":
-        change = int(change[2:])
-        row_changes.append(change)
-    else:
-        change = int(change[2:])
-        column_changes.append(change)
-for i in range(rows):
-    result = []
-    for j in range(columns):
-        result.append(True)
-    canvas.append(result)
-for i in row_changes:
-    for j in range(columns):
-        if canvas[i-1][j] is True:
-            canvas[i-1][j] = False
-        else:
-            canvas[i-1][j] = True
-for i in column_changes:
-    for j in range(rows):
-        if canvas[j][i-1] is True:
-            canvas[j][i-1] = False
-        else:
-            canvas[j][i-1] = True
+rows, columns = int(input()), int(input())
+canvas = [[0]*columns for i in range(rows)]
 counter = 0
-for i in range(rows):
-    for j in canvas[i]:
-        if j is False:
-            counter += 1
+for i in range(int(input())):
+    string = input().split(" ")
+    if string[0] == 'R':
+        ro = int(string[1])-1
+        for j in range(columns):
+            if canvas[ro][j] == 0:
+                canvas[ro][j] = 1
+                counter += 1
+            else:
+                canvas[ro][j] = 0
+                counter -= 1
+    else:
+        col = int(string[1])-1
+        for j in range(rows):
+            if canvas[j][col] == 0:
+                canvas[j][col] = 1
+                counter += 1
+            else:
+                canvas[j][col] = 0
+                counter -= 1
 print(counter)
