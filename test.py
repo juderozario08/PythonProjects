d175106bcdad5fe06cba8716918e0d603a0b0891
Jude Rozario <78745006+juderozario@users.noreply.github.com>
2022-11-07 10:00:42 -0500

    First Commit

--- test.py
+++ test.py
@@ -42,34 +42,7 @@ def oddBananzaComp(ls):
 # for i in range(5):
 #     print(" "*i+x+" "*(9-i*2), x)
 # print(" "*6, x)
-def bowTie(h):
-    print(
-        ''.join(['*'*(2*i + 1) + ' '*2*(h - (2*i + 1)) + '*'*(2*i + 1)+'\n' for i in range(h//2 + 1)]) + ''.join(
-            ['*'*(2*i + 1) + ' '*2*(h - (2*i + 1)) + '*'*(2*i + 1)+'\n' for i in range(h//2 - 1, -1, -1)]))
 
-
-R = []
-vowels = 'aeiou'
-for verse in [[input() for j in range(4)] for i in range(int(input()))]:
-    for line in verse:
-        lastWord = line.split(" ")[-1].lower()
-        result = ''
-        for character in range(len(lastWord)-1, -1, -1):
-            result += lastWord[character]
-            if lastWord[character] in vowels:
-                break
-        R.append(result)
-    if R[0] == R[1] == R[2] == R[3]:
-        print('perfect')
-    elif R[0] == R[1] and R[2] == R[3]:
-        print('even')
-    elif R[0] == R[3] and R[1] == R[2]:
-        print('shell')
-    elif R[0] == R[2] and R[1] == R[3]:
-        print('cross')
-    else:
-        print('free')
-    R.clear()
 
 if __name__ == '__main__':
-    bowTie(int(input()))
+    print()
